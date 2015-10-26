dbca7001cf1a50359ddeeb0b3b5afa32c823a047
Crista Scheuer <cristascheuer@gmail.com>
2015-10-26 16:22:49 -0700

    added value method

--- inventory/Inventory.rb
+++ inventory/Inventory.rb
@@ -5,4 +5,12 @@ class Inventory
 	def add(product)
 		@inventory << product
 	end
+	def value
+		@total_value = 0
+		@inventory.each do |product|
+			value = product.product_info[:price] * product.product_info[:quantity]
+			@total_value += value
+		end
+		puts @total_value
+	end
 end
